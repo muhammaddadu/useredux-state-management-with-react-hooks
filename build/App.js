ac3b9c4ff8d1c9e902451b92f313fe3ccd826077
Muhammad Dadu <muhammad@dadu.co>
2019-04-19 15:30:11 +0100

    dont add build :@

--- build/App.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var React = require("react");
-var ReactDOM = require("react-dom");
-var Hello_1 = require("./components/Hello");
-ReactDOM.render(React.createElement(Hello_1.Hello, { compiler: "Typescript", framework: "React", bundler: "Webpack" }), document.getElementById('root'));
-//# sourceMappingURL=App.js.map
